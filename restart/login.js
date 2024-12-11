e759775613047361481dc86a695c3c1adcaa2247
Chalcepro <glitchtrapsonic@gmail.com>
2024-12-12 00:25:05 +0500

    portfolio commit

--- restart/login.js
+++
@@ -1,8 +0,0 @@
-// const simpleAnim = document.getElementsByClassName("form-text")
-// const simpleForm = document.getElementsByClassName("form-control")
-
-
-// simpleAnim.addEventListener("hover", () => {
-//     event.target.style.opacity = 0
-// }
-// )
